26f2fa3e25e24aa04601706ce67fe555516b631f
yanlei <yanlei@zerozero.cn>
2019-07-13 15:39:39 +0800

    add setenv.sh for ubuntu 18.04

---
+++ setupenv.sh
@@ -0,0 +1,1 @@
+sudo  apt install qtcreator qtbase5-dev cmake make g++ xorg-dev libssl-dev libx11-dev libsodium-dev libgl1-mesa-glx libegl1-mesa libcurl4-openssl-dev libavahi-compat-libdnssd-dev qtdeclarative5-dev libqt5svg5-dev libsystemd-dev -y
